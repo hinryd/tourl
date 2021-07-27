18f9f800b2ac1f4f6f2fc0e575cec3cfd0a9bcc0
Henry <lkhh@protonmail.com>
2021-07-27 17:36:28 +0800

    Fix firebase cert

--- lib/db.ts
+++ lib/db.ts
@@ -2,9 +2,11 @@ import firebase from 'firebase-admin'
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        credential: firebase.credential.cert(
-            JSON.parse(process.env.SERVICE_KEY ?? '')
-        )
+        credential: firebase.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+        })
     })
 }
 
