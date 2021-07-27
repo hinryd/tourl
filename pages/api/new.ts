5acb601820027fae2e1ac7b934bc04a8050a416d
Henry <lkhh@protonmail.com>
2021-07-27 20:15:55 +0800

    Change hashing method and add truncation

--- pages/api/new.ts
+++ pages/api/new.ts
@@ -5,13 +5,13 @@ import db from '../../lib/db'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const url = req.body
     const docRef = db.collection('tourl').doc('default')
-    const digest = createHash('md5').update(url).digest('hex')
-    const dbRes = await docRef.set({ [digest]: url }, { merge: true })
+    const key = createHash('sha1').update(url).digest('base64').slice(0, 8)
+    const dbRes = await docRef.set({ [key]: url }, { merge: true })
 
     if (dbRes) {
         return res.status(200).json({
             error: null,
-            data: digest
+            data: `${process.env.PROD_URL}/${key}`
         })
     } else {
         return res.status(404).json({
