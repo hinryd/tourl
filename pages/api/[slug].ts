e6cd3cc4757c9d8d23a2e7688cbee867a6c74ede
Henry <lkhh@protonmail.com>
2021-07-27 17:15:49 +0800

    Fix type guard slug

--- pages/api/[slug].ts
+++ pages/api/[slug].ts
@@ -3,11 +3,14 @@ import db from '../../lib/db'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { slug } = req.query
+    const isStr = (str: string | string[]): str is string => {
+        return (str as string) !== undefined
+    }
     const docRef = db.collection('tourl').doc('default')
     const doc = await docRef.get()
     const data = doc.data()
 
-    if (doc.exists && data) {
+    if (doc.exists && data && isStr(slug)) {
         return res.status(200).json({
             error: null,
             data: data[slug]
